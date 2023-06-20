4ad6e68cce2839d55fe75a6f514a1b751afda5b6
gllnsv <agalinka536@mail.ru>
2023-06-20 14:46:35 +0300

    added print hello!

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def strcounter(s):
     syms_counter = {}
     for sym in s:
         syms_counter[sym] = syms_counter.get(sym, 0) + 1
-
+    print('hello!')
     print(syms_counter)
 
 strcounter('aaabca')
